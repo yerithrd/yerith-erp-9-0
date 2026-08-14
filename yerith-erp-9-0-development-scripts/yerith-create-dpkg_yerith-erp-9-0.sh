465d37d097c710b18ed2979e0f0d2600f594c4de
Pr. Prof. Dr.-Ing. Xavier Noundou <Yerith.xavier@gmail.com>
2026-08-14 14:40:44 -0400

    UPDATED.

--- yerith-erp-9-0-development-scripts/yerith-create-dpkg_yerith-erp-9-0.sh
+++ yerith-erp-9-0-development-scripts/yerith-create-dpkg_yerith-erp-9-0.sh
@@ -202,7 +202,8 @@ ${ECHO} -e "${YERITH_ERP_3_0_DEB_PACKAGE_INFORMATION}$(cat ${TARGET_INSTALLATION
 
 YERITH_ERP_3_0_POSTRM_STR="#!/bin/bash
 sed -i '/YERITH_ERP_3_0_HOME_FOLDER/d' /etc/environment
-sed -i '/YERITH_ERP_3_0_SYSTEM_DAEMON_HOME_FOLDER/d' /etc/environment"
+sed -i '/YERITH_ERP_3_0_SYSTEM_DAEMON_HOME_FOLDER/d' /etc/environment
+sed -i '/EXP_QT_SCALE_FACTOR/d' /etc/environment"
 
 ${ECHO} -e "${YERITH_ERP_3_0_POSTRM_STR}$(cat ${TARGET_INSTALLATION_FOLDER}/DEBIAN/postrm)" > ${TARGET_INSTALLATION_FOLDER}/DEBIAN/postrm
 
@@ -212,6 +213,7 @@ chmod 755 ${TARGET_INSTALLATION_FOLDER}/DEBIAN/postrm
 YERITH_ERP_3_0_POSTINST_STR="#!/bin/bash
 echo -e \"export YERITH_ERP_3_0_HOME_FOLDER=/opt/${YERITH_ERP_3_0_BINARY_NAME}\" >> /etc/environment
 echo -e \"export YERITH_ERP_3_0_SYSTEM_DAEMON_HOME_FOLDER=/opt/yerith-erp-9-0-system-daemon\" >> /etc/environment
+echo -e \"export EXP_QT_SCALE_FACTOR=1.32\" >> /etc/environment
 touch /opt/${YERITH_ERP_3_0_BINARY_NAME}/yerith-erp-9-0.log
 /opt/${YERITH_ERP_3_0_BINARY_NAME}/bin/yerith-erp-9-0-configure-mysql-server.sh
 /opt/${YERITH_ERP_3_0_BINARY_NAME}/bin/yerith-erp-9-0-standalone-setup-database-and-user.sh
